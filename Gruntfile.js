010f29ca535a55678c4d75eb514c6c0926fda321
nicklupu <nick@ftddigital.com>
2017-06-19 10:24:04 +0100

    Additional pages (all games, category, news post and software review)

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,10 +32,10 @@ module.exports = function (grunt) {
                     // destination				// source file
                     'app/css/app.css':			'source/sass/styles.scss',
                     // other casnos
-                    'app/css/amphome.css':			'source/sass/ampstyles.scss',
-                    'app/css/ampcasino.css':			'source/sass/casinoampstyles.scss',
-                    'app/css/ampgame.css':			'source/sass/gameampstyles.scss',
-                    'app/css/casinos/betfair.css':	'source/sass/partials/casinos/betfair.scss',
+                    //'app/css/amphome.css':			'source/sass/ampstyles.scss',
+                    //'app/css/ampcasino.css':			'source/sass/casinoampstyles.scss',
+                    //'app/css/ampgame.css':			'source/sass/gameampstyles.scss',
+                    //'app/css/casinos/betfair.css':	'source/sass/partials/casinos/betfair.scss',
                     //'app/casino02/css/app.css':	'source/casino02/sass/styles.sass'
 
                 },
