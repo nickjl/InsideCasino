10bdd50510fe74dfa26b37918c65b4db10e5f73d
nicklupu <njlupu@gmail.com>
2017-07-10 17:13:37 +0100

    Bonus card tweaks

--- app/assets/js/bxSlider.js
+++ app/assets/js/bxSlider.js
@@ -66,14 +66,20 @@ $('.viewMoreBonuses').click(function() {
 $(function() {
     var BLUR_RADIUS = 100;
     var canvas = document.querySelector('[data-canvas]');
-    var canvasContext = canvas.getContext('2d');
-    var image = new Image();
-    image.src = document.querySelector('[data-canvas-image]').src;
-    var drawBlur = function() {
-        var w = canvas.width;
-        var h = canvas.height;
-        canvasContext.drawImage(image, 0, 0, w, h);
-        stackBlurCanvasRGBA('heroCanvas', 0, 0, w, h, BLUR_RADIUS);  };
-    image.onload = function() {    drawBlur();  }
+    if(canvas) {
+        var canvasContext = canvas.getContext('2d');
+        var image = new Image();
+        image.src = document.querySelector('[data-canvas-image]').src;
+        var drawBlur = function () {
+            var w = canvas.width;
+            var h = canvas.height;
+            canvasContext.drawImage(image, 0, 0, w, h);
+            stackBlurCanvasRGBA('heroCanvas', 0, 0, w, h, BLUR_RADIUS);
+        };
+        image.onload = function () {
+            drawBlur();
+        }
+    }
 });
+
 
